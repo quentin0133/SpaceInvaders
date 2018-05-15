8d3c31fd93119e69c99c141dcf2d863d257dcb9c
quentin <quentin.yahia@etu.unilim.fr>
2018-05-15 12:59:09 +0200

    test 2

--- src/main/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -3,6 +3,6 @@ package fr.unilim.iut.spaceinvaders.utils;
 @SuppressWarnings("serial")
 public class HorsEspaceJeuException extends RuntimeException {
 	public HorsEspaceJeuException(String message) {
-	    super(message);
+	    super(message); 
     }
 }
