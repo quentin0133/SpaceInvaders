9f40fe4ac79ac41b8a6e371b1bff05f0b93a9f92
iut <iut@10.0.2.15>
2018-05-22 18:58:49 +0200

    Vaisseau avec dimension et l'empêcher de déborder de l'espace de jeu

--- src/main/java/fr/unilim/iut/spacerinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spacerinvaders/SpaceInvaders.java
@@ -2,6 +2,7 @@ package fr.unilim.iut.spacerinvaders;
 
 import org.junit.Before;
 
+import fr.unilim.iut.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvaders {
@@ -49,6 +50,11 @@ public class SpaceInvaders {
 	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
 		if (!estDansEspaceJeu(x, y))
 			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		if ( !estDansEspaceJeu(x+longueur-1,y))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers la droite à cause de sa longueur");
+		if (!estDansEspaceJeu(x,y-hauteur+1))
+			throw new DebordementEspaceJeuException("Le vaisseau déborde de l'espace jeu vers le bas à cause de sa hauteur");
 
 		vaisseau = new Vaisseau(longueur, hauteur);
 		vaisseau.positionner(x, y);
