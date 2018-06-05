f6b3b22484caa3fd607b3b2ba15f0526a3d87238
Quentin Yahia <38751481+quentin0133@users.noreply.github.com>
2018-06-05 22:10:32 +0200

    Delete MissileException.java

--- spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/util/MissileException.java
+++
@@ -1,9 +0,0 @@
-package fr.unilim.iut.spaceinvaders.util;
-
-@SuppressWarnings("serial")
-public class MissileException extends RuntimeException {
-
-	public MissileException(String message) {
-		super(message);
-	}
-}
