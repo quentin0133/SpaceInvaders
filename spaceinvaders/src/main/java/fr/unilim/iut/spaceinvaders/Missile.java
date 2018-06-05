5f2d27a753b0b6c362e47bc0948170fb81536ecf
Quentin Yahia <38751481+quentin0133@users.noreply.github.com>
2018-06-05 22:08:32 +0200

    Delete Missile.java

--- spaceinvaders/src/main/java/fr/unilim/iut/spaceinvaders/Missile.java
+++
@@ -1,9 +0,0 @@
-package fr.unilim.iut.spaceinvaders;
-
-public class Missile extends Sprite {
-
-	public Missile(Dimension dimension, Position origine, int vitesse) {
-		super(dimension, origine, vitesse);
-	}
-	
-}
